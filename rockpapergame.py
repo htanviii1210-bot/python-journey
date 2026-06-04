9d0d9c347982b325943032d2c5898861ef229be0
Tanvi Hegde <htanviii1210@gmail.com>
2026-06-04 14:02:51 +0530

    Add Rock Paper Scissors game

---
+++ rockpapergame.py
@@ -0,0 +1,17 @@
+import random
+choices = ["Rock", "Paper", "Scissors"]
+print("Welcome to the rock paper scissor game!")
+print("Enter 0 for rock, 1 for paper and 2 for scissor!")
+ch=int(input("Enter your choice:"))
+compch=random.randint(0,2)
+if 0 <= ch <len(choices):
+    print(f"Computer chose {choices[compch]}")
+    print(f"U chose {choices[ch]}")
+    if (ch==0 and compch==1) or (ch==1 and compch==2) or (ch==2 and compch==0):
+        print("You lose")
+    elif (ch==0 and compch==2) or (ch==1 and compch==0) or (ch==2 and compch==1):
+        print("You win")
+    elif (ch==0 and compch==0) or (ch==1 and compch==1) or(ch==2 and compch==2):
+        print("Its a draw!")
+else:
+    print("Invalid input!")
--- todo.py
+++ todo.py
@@ -24,34 +24,33 @@ while True:
         print("Thanks for using the To-Do List App!")
         break
     elif choice=="1":
-        task1=str(input("Enter the task:"))
+        task1=input("Enter the task:")
         tasks.append(task1)
         print("Task added\n")
         for i, task in enumerate(tasks,start=1):
-            print(f"{i}.{task}")
+            print(f"{i}. {task}")
     elif choice=="2":
         if not tasks:
             print("No tasks available")
         else:
             for i, task in enumerate(tasks,start=1):
-                print(f"{i}.{task}")
+                print(f"{i}. {task}")
     elif choice=="3":
         if not tasks:
             print("No tasks left to be deleted!")
         else:
             print("Current List")
             for i, task in enumerate(tasks,start=1):
-                print(f"{i}.{task}")
+                print(f"{i}. {task}")
             ch=int(input("Enter the task number:"))
             ch=ch-1
-            if ch<len(tasks) and ch>=0:
-                print("Before deletion")
-                for i, task in enumerate(tasks,start=1):
-                    print(f"{i}.{task}")
+            if 0 <= ch < len(tasks):
                 tasks.pop(ch)
-                print("After deletion")
+                print("Task deleted successfully!")
                 for i, task in enumerate(tasks,start=1):
-                    print(f"{i}.{task}")
+                    print(f"{i}. {task}")
+            else:
+                print("Invalid input!")
         
       
     else:
