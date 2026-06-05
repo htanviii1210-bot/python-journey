5fdeb8ed5692e89726c2b98ebd2f9d3b79e347bc
Tanvi Hegde <htanviii1210@gmail.com>
2026-06-05 12:56:33 +0530

    Update rock paper scissors project

--- rockpapergame.py
+++ rockpapergame.py
@@ -3,8 +3,8 @@ choices = ["Rock", "Paper", "Scissors"]
 print("Welcome to the rock paper scissor game!")
 print("Enter 0 for rock, 1 for paper and 2 for scissor!")
 ch=int(input("Enter your choice:"))
-compch=random.randint(0,2)
 if 0 <= ch <len(choices):
+    compch=random.randint(0,2)
     print(f"Computer chose {choices[compch]}")
     print(f"U chose {choices[ch]}")
     if (ch==0 and compch==1) or (ch==1 and compch==2) or (ch==2 and compch==0):
