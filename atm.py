84d280a29b3467d536409fe9e068cd86d3a3bfef
Tanvi Hegde <htanviii1210@gmail.com>
2026-06-06 13:45:19 +0530

    Add calculator using functions and improve ATM simulator

--- atm.py
+++ atm.py
@@ -13,11 +13,14 @@ while True:
     elif ch == "2":
         amt = int(input("Enter the amount u want to deposit:"))
         balance+=amt
+        print(f"₹{amt} deposited successfully")
+        print(f"Your current balance is ₹{balance}")
     elif ch == "3":
         amt = int(input("Enter the amount u want to withdraw:"))
         if amt <= balance:
             balance= balance-amt
-            print(f"Your current balance is {balance}")
+            print(f"₹{amt} withdrawn successfully")
+            print(f"Your current balance is ₹{balance}")
         else:
             print("Insufficient Balance")
     elif ch =="1":
